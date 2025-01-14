980ddad062c4e7a7db6d953b5a63e36746d3e40e
Sayem Kamal <sayemkamal12@gmail.com>
2025-01-13 22:22:57 -0500

    Add score, win announcement functionality

--- script.js
+++ script.js
@@ -16,38 +16,54 @@ function playGame(choice){
     const humanChoice = choice.toLowerCase();
     const computerChoice = getComputerChoice();
     
-    function playRound(humanChoice,computerChoice){
+    function playRound(humanChoice,computerChoice){   
+        const resultElement = document.querySelector("#result");
+        const scoreElement = document.querySelector("#score");     
         switch(humanChoice){
             case "rock":
                 if(computerChoice == "scissors"){
                     humanScore++;
-                    console.log("You win! Rock beats scissors.")
+                    resultElement.textContent = "You win! Rock beats scissors.";
                 }
                 else if(computerChoice == "paper"){
                     computerScore++;
-                    console.log("You lose! Paper beats rock.")
+                    resultElement.textContent = "You lose! Paper beats rock.";
                 }
                 break;
             case "scissors":
                 if(computerChoice == "paper"){
                     humanScore++;
-                    console.log("You win! Scissors beats paper.")
+                    resultElement.textContent = "You win! Scissors beats paper.";
                 }
                 else if(computerChoice == "rock"){
                     computerScore++;
-                    console.log("You lose! Rock beats scissors.")
+                    resultElement.textContent = "You lose! Rock beats scissors.";
                 }
                 break;
             case "paper":
                 if(computerChoice == "rock"){
                     humanScore++;
-                    console.log("You win! Paper beats rock.")
+                    resultElement.textContent = "You win! Paper beats rock.";
                 }
                 else if(computerChoice == "scissors"){
                     computerScore++;
-                    console.log("You lose! Scissors beats paper.")
+                    resultElement.textContent = "You lose! Scissors beats paper.";
                 }
                 break;
+        }
+        scoreElement.textContent = `Score: You: ${humanScore} Computer: ${computerScore}`;
+        if(humanScore == 5 || computerScore == 5){
+            const scoreNode = document.querySelector("#score");
+            const res = document.createElement("div");
+            if (humanScore > computerScore){
+                res.textContent = "You win!"
+            }
+            else if(humanScore < computerScore) {
+                res.textContent = "You lose..."
+            }
+            humanScore = 0;
+            computerScore = 0;
+            scoreNode.after(res);
         }
     }
     playRound(humanChoice,computerChoice);
